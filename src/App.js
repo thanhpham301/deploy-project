0a094ba45ea6e206379212b4754a064996dab405
thanhpham301 <thanhpham3011993@gmail.com>
2023-02-07 20:00:14 +0700

    2

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 // import './App.css';
 import Header from './Header/Header';
+import Body from './Body/Body';
+
 
 function App() {
   return (
     <div>
       <Header />
+      <Body />
     </div>
   );
 }
---
+++ src/Body/Body.js
@@ -0,0 +1,11 @@
+import Nav from './Nav';
+
+function Body () {
+    return (
+        <div>
+            <Nav />
+        </div>
+    )
+}
+
+export default Body;
